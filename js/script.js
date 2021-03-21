198901c35898dfaf033c6a09837e065e69a94b95
Pavel <55561686+Witchbanee@users.noreply.github.com>
2021-03-21 17:48:55 +0300

    Обновил Html, Js, Css, Img

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 let burger = document.querySelector(".nav-button");
 let burgerLine = document.querySelector(".nav__line");
@@ -15,30 +14,3 @@ burger.addEventListener("click", function (evt) {
 });
 
 
-=======
-let burger = document.querySelector(".nav-button");
-let burgerLine = document.querySelector(".nav__line");
-let burgerOpenMenu = document.querySelector(".navigation__main");
-let body = document.querySelector("body");
-
-
-burger.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    burgerOpenMenu.classList.add("navigation--mobile");
-    body.classList.add("position");
-    
-    burgerLine.classList.remove("nav__line");
-    burgerLine.classList.remove("nav__line");
-
-    burgerLine.classList.add("nav__line--open");
-    burgerLine.classList.add("nav__line--open");
-    
-});
-
-
-
-// close.addEventListener("click", function (evt) {
-//     evt.preventDefault();
-//     popup.classList.remove("modal-show");
-// });
->>>>>>> 802ea0a23b8a2e77a3e7a3d425b29bdaa48f41fe
