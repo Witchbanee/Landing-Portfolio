cdb171474dc87f5a1bfd9e074a360d0513ed435f
Pavel <danshin-pavel@mail.ru>
2021-03-21 17:35:41 +0300

    Обновил js, css, немного поправил html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 let burger = document.querySelector(".nav-button");
 let burgerLine = document.querySelector(".nav__line");
 let burgerOpenMenu = document.querySelector(".navigation__main");
@@ -6,20 +7,10 @@ let body = document.querySelector("body");
 
 burger.addEventListener("click", function (evt) {
     evt.preventDefault();
-    burgerOpenMenu.classList.add("navigation--mobile");
-    body.classList.add("position");
-    
-    burgerLine.classList.remove("nav__line");
-    burgerLine.classList.remove("nav__line");
-
-    burgerLine.classList.add("nav__line--open");
-    burgerLine.classList.add("nav__line--open");
-    
+    burgerOpenMenu.classList.toggle("navigation--mobile");
+    body.classList.toggle("position");
+    burgerLine.classList.toggle("nav__line");
+    burgerLine.classList.toggle("nav__line--open");
 });
 
 
-
-// close.addEventListener("click", function (evt) {
-//     evt.preventDefault();
-//     popup.classList.remove("modal-show");
-// });
