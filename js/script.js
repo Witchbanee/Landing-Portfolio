70d528c49b832367a0c8466bb05641b74b8b41ba
Pavel <55561686+Witchbanee@users.noreply.github.com>
2021-08-12 19:12:20 +0300

    Add files via upload
    Небольшие правки

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let burger = document.querySelector(".nav-button");
 let burgerLine = document.querySelector(".nav__line");
 let burgerOpenMenu = document.querySelector(".navigation__main");
@@ -7,10 +6,8 @@ let body = document.querySelector("body");
 
 burger.addEventListener("click", function (evt) {
     evt.preventDefault();
-    burgerOpenMenu.classList.toggle("navigation--mobile");
     body.classList.toggle("position");
+    burgerOpenMenu.classList.toggle("navigation--mobile");
+    burger.classList.toggle("nav-close");
     burgerLine.classList.toggle("nav__line");
-    burgerLine.classList.toggle("nav__line--open");
 });
-
-
